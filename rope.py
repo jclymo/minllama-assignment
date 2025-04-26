c97c966222302dfaf5868aba1029bf7222776ad3
Jude <judith.clymo@gmail.com>
2025-04-25 23:58:42 -0700

    rope

--- rope.py
+++ rope.py
@@ -50,26 +50,34 @@ def apply_rotary_emb(
 
     _, seqlen, _, _ = query.shape
     device = query.device
-    # todo
-    #
     # Please refer to slide 22 in https://phontron.com/class/anlp2024/assets/slides/anlp-05-transformers.pdf
     # and Section 3 in https://arxiv.org/abs/2104.09864.
 
     # reshape xq and xk to match the complex representation
-    query_real, query_imag = query.float().reshape(query.shape[:-1] + (-1, 2)).unbind(-1)
+    query_real, query_imag = query.float().reshape(query.shape[:-1] + (-1, 2)).unbind(-1) # (bs, seqlen, n_local_heads, head_dim//2)
     key_real, key_imag = key.float().reshape(key.shape[:-1] + (-1, 2)).unbind(-1)
     # This separates each query/key vector into its odd and even indices (assuming *one-indexing*).
     # query_real contains q_1, q_3, q_5, ... and query_imag contains q_2, q_4, q_6, ...
 
     # First, compute the trigonometric values in the second and fourth columns in
     # slide 22 (linked above).
+    dim_t = torch.arange(0, head_dim // 2, device=device).unsqueeze(0) # (1, head_dim//2)
+    thetas = 10_000.0 ** (-2 * dim_t / head_dim) # (1, head_dim//2)
+    pos = torch.arange(0, seqlen, device=device).unsqueeze(-1) # (seqlen, 1)
+    thetas = pos * thetas # (seqlen, head_dim//2)
+    cos_thetas = torch.cos(thetas).unsqueeze(0).unsqueeze(2) # (1, seqlen, 1, head_dim//2)
+    sin_thetas = torch.sin(thetas).unsqueeze(0).unsqueeze(2) # (1, seqlen, 1, head_dim//2)
 
     # Then, combine these trigonometric values with the tensors query_real, query_imag,
     # key_real, and key_imag.
+    # shape (bs, seqlen, n_local_heads, head_dim//2)
+    query_real_out = query_real * cos_thetas - query_imag * sin_thetas
+    query_imag_out = query_imag * cos_thetas + query_real * sin_thetas
+    key_real_out = key_real * cos_thetas - key_imag * sin_thetas
+    key_imag_out = key_imag * cos_thetas + key_real * sin_thetas
 
-    raise NotImplementedError
+    query_out = torch.stack([query_real_out, query_imag_out], dim=-1).flatten(-2, -1) # (bs, seqlen, n_local_heads, head_dim)
+    key_out = torch.stack([key_real_out, key_imag_out], dim=-1).flatten(-2, -1)
 
-    query_out = None
-    key_out = None
     # Return the rotary position embeddings for the query and key tensors
     return query_out, key_out
