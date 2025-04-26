6679f86b7e4ce738bc9917e446adf49c7b8ad094
Jude <judith.clymo@gmail.com>
2025-04-25 20:50:40 -0700

    implement AdamW

--- optimizer.py
+++ optimizer.py
@@ -2,6 +2,7 @@ from typing import Callable, Iterable, Tuple
 
 import torch
 from torch.optim import Optimizer
+import math
 
 
 class AdamW(Optimizer):
@@ -37,24 +38,33 @@ class AdamW(Optimizer):
                 grad = p.grad.data
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
-
-                raise NotImplementedError()
-
+                
                 # State should be stored in this dictionary
                 state = self.state[p]
-
-                # Access hyperparameters from the `group` dictionary
-                alpha = group["lr"]
+                if state.get("step", None) is None:
+                    state["step"] = 0
+                state["step"] += 1
 
                 # Update first and second moments of the gradients
+                if state.get("m", None) is None:
+                    state["m"] = torch.zeros_like(p.data)
+                if state.get("v", None) is None:
+                    state["v"] = torch.zeros_like(p.data)
+                beta1, beta2 = group["betas"]
+                state["m"].mul_(beta1).add_(grad, alpha=(1 - beta1))
+                state["v"].mul_(beta2).addcmul_(grad, grad, value=(1 - beta2))
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
 
                 # Update parameters
+                alpha = group["lr"]
+                alpha_t = alpha * math.sqrt(1 - beta2 ** state["step"]) / (1 - beta1 ** state["step"])
+                p.data -= alpha_t * state["m"] / (torch.sqrt(state["v"]) + group["eps"])
 
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+                p.data -= alpha * group["weight_decay"] * p.data
 
         return loss
